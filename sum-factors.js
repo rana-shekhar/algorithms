7e1603b317d6361405058b6b9ba3a27bab28a8f8
rana-shekhar <shekharrana1011@gmail.com>
2023-05-12 13:25:19 +0530

    added sum factor program

---
+++ sum-factors.js
@@ -0,0 +1,45 @@
+/**
+ * 
+ * Given a positive integer n, find the sum of all integers in the range [1, n] inclusive that are divisible by 3, 5, or 7.
+
+Return an integer denoting the sum of all numbers in the given range satisfying the constraint.
+
+ 
+
+Example 1:
+
+Input: n = 7
+Output: 21
+Explanation: Numbers in the range [1, 7] that are divisible by 3, 5, or 7 are 3, 5, 6, 7. The sum of these numbers is 21.
+Example 2:
+
+Input: n = 10
+Output: 40
+Explanation: Numbers in the range [1, 10] that are divisible by 3, 5, or 7 are 3, 5, 6, 7, 9, 10. The sum of these numbers is 40.
+Example 3:
+
+Input: n = 9
+Output: 30
+Explanation: Numbers in the range [1, 9] that are divisible by 3, 5, or 7 are 3, 5, 6, 7, 9. The sum of these numbers is 30.
+ 
+ * 
+ * 
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var sumOfMultiples = function(n) {
+    let arr = [];
+    for (let num = 1; num <= n; num++) {
+        if(num % 3 === 0 || num % 5  === 0 || num % 7 === 0) {
+            arr.push(num);     
+        }
+    }
+    let sum = 0;
+    for (let num = 0; num <arr.length; num++){
+        sum = sum+arr[num]; 
+    }
+    return sum;
+};
